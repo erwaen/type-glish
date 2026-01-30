48d2200075548ca20a2bf8dcb4d9d6f7afa7d25e
Erik Wasmosy cachy <erikwasmosy98@gmail.com>
2026-01-30 10:28:59 -0500

    format

--- main.go
+++ main.go
@@ -8,27 +8,27 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-var  (
-	greyStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("204"))
-	typedStyle= lipgloss.NewStyle().Foreground(lipgloss.Color("111"))
+var (
+	greyStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("204"))
+	typedStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("111"))
 	cursorStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("222"))
 )
 
 type model struct {
-	cursor   int
+	cursor int
 	target string
-	typed string
+	typed  string
 }
 
 func initialModel() model {
 	return model{
 		target: "Hey, do you want to eat pizza today?",
-		typed: "",
+		typed:  "",
 	}
 }
 
 func (m model) Init() tea.Cmd {
-	return tea.EnterAltScreen 
+	return tea.EnterAltScreen
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -41,17 +41,17 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			return m, tea.Quit
 
 		case "backspace":
-			if m.cursor > 0{
+			if m.cursor > 0 {
 				m.cursor--
 				m.typed = m.typed[:len(m.typed)-1]
 			}
 
 		default:
-			if m.cursor >= len(m.target){
+			if m.cursor >= len(m.target) {
 				return m, nil
 			}
 			expected := string(m.target[m.cursor])
-			if msg.String() == expected{
+			if msg.String() == expected {
 				m.typed += msg.String()
 				m.cursor++
 			}
@@ -62,32 +62,29 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, nil
 }
 
-func (m model) View() string{
+func (m model) View() string {
 	if m.cursor >= len(m.target) {
-        return "You finished! Press q to quit.\n"
+		return "You finished! Press q to quit.\n"
 	}
-	s:= "Type to learn!\n\n"
+	s := "Type to learn!\n\n"
 
+	completed := typedStyle.Render(m.target[:m.cursor])
+	cursorChar := cursorStyle.Render(string(m.target[m.cursor]))
+	remaining := greyStyle.Render(m.target[m.cursor+1:])
 
-	completed:= typedStyle.Render(m.target[:m.cursor])
-	cursorChar:= cursorStyle.Render(string(m.target[m.cursor]))
-	remaining:= greyStyle.Render(m.target[m.cursor+1:])
+	s += fmt.Sprintf("%s[%s]%s\n", completed, cursorChar, remaining)
 
-			
-
-	s+= fmt.Sprintf("%s[%s]%s\n", completed, cursorChar,remaining) 
-
-	s+= "\nPress q to quit.\n"
+	s += "\nPress q to quit.\n"
 
 	return s
 }
 
-func main(){
-		p:= tea.NewProgram(initialModel())
+func main() {
+	p := tea.NewProgram(initialModel())
 
-		if _,err:= p.Run(); err != nil{
-			fmt.Printf("Alas, there's been an error: %v", err)
-			os.Exit(1)
-		}	
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
 
 }
